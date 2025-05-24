b57ab2928a724fd5d84291666bca9b05c2657ec9
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-05-24 12:41:20 +0200

    Add new design for PokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -37,62 +37,124 @@ const PokemonCard: React.FC<Props> = ({ name, url }) => {
 
   const fnConvert = (value: number): string => `${value / 10}`;
 
-  const setTypeColor = (type: string, id: number) => {
+  const setTypeColor = (type: string) => {
     let colorType: string | null = null;
+    // switch (type) {
+    //   case "normal":
+    //     colorType = "#9fa19f";
+    //     break;
+    //   case "fighting":
+    //     colorType = "#ff8000";
+    //     break;
+    //   case "flying":
+    //     colorType = "#81b9ef";
+    //     break;
+    //   case "poison":
+    //     colorType = "#9141cb";
+    //     break;
+    //   case "ground":
+    //     colorType = "#915121";
+    //     break;
+    //   case "rock":
+    //     colorType = "#afa981";
+    //     break;
+    //   case "bug":
+    //     colorType = "#91a119";
+    //     break;
+    //   case "ghost":
+    //     colorType = "#704170";
+    //     break;
+    //   case "steel":
+    //     colorType = "#60a1b8";
+    //     break;
+    //   case "fire":
+    //     colorType = "#e62829";
+    //     break;
+    //   case "water":
+    //     colorType = "#2980ef";
+    //     break;
+    //   case "grass":
+    //     colorType = "#3fa129";
+    //     break;
+    //   case "electric":
+    //     colorType = "#fac000";
+    //     break;
+    //   case "psychic":
+    //     colorType = "#ef4179";
+    //     break;
+    //   case "ice":
+    //     colorType = "#3dcef3";
+    //     break;
+    //   case "dragon":
+    //     colorType = "#5060e1";
+    //     break;
+    //   case "dark":
+    //     colorType = "#624d4e";
+    //     break;
+    //   case "fairy":
+    //     colorType = "#ef70ef";
+    //     break;
+    //   case "stellar":
+    //     colorType = "#40b5a5";
+    //     break;
+    //   case "unknown":
+    //     colorType = "#68a090";
+    //     break;
+    // }
     switch (type) {
       case "normal":
-        colorType = "#9fa19f";
+        colorType = "#A8A77A";
         break;
       case "fighting":
-        colorType = "#ff8000";
+        colorType = "#C22E28";
         break;
       case "flying":
-        colorType = "#81b9ef";
+        colorType = "#A98FF3";
         break;
       case "poison":
-        colorType = "#9141cb";
+        colorType = "#A33EA1";
         break;
       case "ground":
-        colorType = "#915121";
+        colorType = "#E2BF65";
         break;
       case "rock":
-        colorType = "#afa981";
+        colorType = "#B6A136";
         break;
       case "bug":
-        colorType = "#91a119";
+        colorType = "#A6B91A";
         break;
       case "ghost":
-        colorType = "#704170";
+        colorType = "#735797";
         break;
       case "steel":
-        colorType = "#60a1b8";
+        colorType = "#B7B7CE";
         break;
       case "fire":
-        colorType = "#e62829";
+        colorType = "#EE8130";
         break;
       case "water":
-        colorType = "#2980ef";
+        colorType = "#6390F0";
         break;
       case "grass":
-        colorType = "#3fa129";
+        colorType = "#7AC74C";
         break;
       case "electric":
-        colorType = "#fac000";
+        colorType = "#F7D02C";
         break;
       case "psychic":
-        colorType = "#ef4179";
+        colorType = "#F95587";
         break;
       case "ice":
-        colorType = "#3dcef3";
+        colorType = "#96D9D6";
         break;
       case "dragon":
-        colorType = "#5060e1";
+        colorType = "#6F35FC";
         break;
       case "dark":
-        colorType = "#624d4e";
+        colorType = "#705746";
         break;
       case "fairy":
-        colorType = "#ef70ef";
+        colorType = "#D685AD";
         break;
       case "stellar":
         colorType = "#40b5a5";
@@ -101,13 +163,7 @@ const PokemonCard: React.FC<Props> = ({ name, url }) => {
         colorType = "#68a090";
         break;
     }
-    return (
-      <Chip
-        key={id}
-        label={type}
-        sx={{ background: colorType, color: "white", margin: "0 4px" }}
-      />
-    );
+    return colorType;
   };
 
   useEffect(() => {
@@ -118,18 +174,25 @@ const PokemonCard: React.FC<Props> = ({ name, url }) => {
   }, [url]);
 
   return (
-    <Card>
+    <>
       {pokemonDetails ? (
-        <>
+        <Card
+          sx={{
+            background: `radial-gradient(circle at center bottom, ${setTypeColor(
+              pokemonDetails.types[0].type.name
+            )} 0%, rgba(0, 0, 0, 0.9) 60%)`,
+            padding: 3,
+          }}>
           <CardMedia
             component="img"
+            draggable={false}
             image={pokemonDetails.sprites.other.home.front_default}
           />
-          <Box textAlign="left" p={3}>
-            <Typography variant="h5" textTransform="capitalize" gutterBottom>
+          <Box textAlign="left" color="white" pt={5}>
+            <Typography variant="h4" textTransform="capitalize" gutterBottom>
               {name}
             </Typography>
-            <Box className="basic-info" display="flex" mb={2}>
+            <Box className="basic-info" display="flex" mb={3}>
               <Typography variant="body1" mr={1}>
                 height: {fnConvert(pokemonDetails.height)}m
               </Typography>
@@ -138,16 +201,24 @@ const PokemonCard: React.FC<Props> = ({ name, url }) => {
               </Typography>
             </Box>
             <Box className="type" textTransform="capitalize">
-              {pokemonDetails.types.map((type, id) =>
-                setTypeColor(type.type.name, id)
-              )}
+              {pokemonDetails.types.map(({ type }, id) => (
+                <Chip
+                  key={id}
+                  label={type.name}
+                  sx={{
+                    background: setTypeColor(type.name),
+                    color: "white",
+                    margin: "0 4px",
+                  }}
+                />
+              ))}
             </Box>
           </Box>
-        </>
+        </Card>
       ) : (
         <CircularProgress color="inherit" />
       )}
-    </Card>
+    </>
   );
 };
 
